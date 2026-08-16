0fb8e6852b1c96b7cb4ac905b005e6a5b2bd91ad
leilanyembo750-del <leilanyembo750@gmail.com>
2026-08-16 20:59:10 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://pehpwrlvafreyiiqauci.supabase.co";
-const SUPABASE_KEY = sb_publishable_wtfgfwA_r1PrKFmQoUWkug_jO9tbu53
+const SUPABASE_KEY = "sb_publishable_wtfgfwA_r1PrKFmQoUWkug_jO9tbu53"
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
